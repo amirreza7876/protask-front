66844e518f1e40893f38ea00d84d473ba7f9eeb9
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 13:47:31 +0430

    inline return

--- src/utils/searchForRoom.js
+++ src/utils/searchForRoom.js
@@ -2,7 +2,7 @@ import api from "../api";
 
 const searchForRoom = async (e, roomString) => {
 	e.preventDefault()
-	const response = await api.post('/rooms/join-requests/', {request_string: roomString})
+	return await api.post('/rooms/join-requests/', {request_string: roomString})
 }
 
 export default searchForRoom;
