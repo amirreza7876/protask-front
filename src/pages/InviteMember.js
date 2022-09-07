f98aa7d95602a553542f12cc2f7250c7b08538f5
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-08 02:03:58 +0430

    create invite functionality

--- src/pages/InviteMember.js
+++ src/pages/InviteMember.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import handleInvite from "../utils/handleInvite";
 import {useLocation} from "react-router-dom";
+import handleInviteMember from "../utils/handleInviteMember";
 
 function InviteMember(props) {
 	const location = useLocation()
-	const state = location.state
+	const {requestString} = location.state
+	// const {} = state
 	const [username, setUsername] = useState('');
 	return (
 		<div>
-			<form onSubmit={e => handleInvite(e)}>
+			<form onSubmit={e => handleInviteMember(e, username, requestString)}>
 				<input type="text" value={username} placeholder={'Username or Email'}
 					   onChange={e => setUsername(e.target.value)} name="" id=""/>
 				<input type="submit" value={'Invite'} name="" id=""/>
