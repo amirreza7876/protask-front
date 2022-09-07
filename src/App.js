9d930debb670a2e410db64208ff934f2b38dae57
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 13:46:43 +0430

    show username in home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import NavigationBar from "./components/NavigationBar";
+import {useEffect, useRef, useState} from "react";
+import getUserName from "./apiCalls/getUserName";
 
 function App() {
+	const [username, setUsername] = useState();
+
+	useEffect(() => {
+		getUserName(setUsername)
+	}, []);
+
 	return (
 		<div className="App">
-			App Page
+			Welcome {username}
 			<NavigationBar/>
 		</div>
 	);
