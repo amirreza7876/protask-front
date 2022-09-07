65fa6a9b0ace13abba5f630e25069f247e3f4b0d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-08 02:06:55 +0430

    create function for user to accept or reject invites

--- src/utils/handleInvite.js
+++ src/utils/handleInvite.js
@@ -1,5 +1,18 @@
-const handleInvite = async () => {
-	alert('complete this part')
+import api from "../api";
+
+const handleInvite = async (event, type, invite) => {
+	event.preventDefault()
+	const fromRoom = invite.from_room.request_string
+	const forUser = invite.for_user
+	const inviteId = invite.id
+	switch (type) {
+		case'accept':
+			await api.post('/rooms/invite/', {fromRoom, forUser, inviteId, accepted: true})
+			break
+		case 'reject':
+			await api.post('/rooms/invite/', {fromRoom, forUser, inviteId, accepted: false})
+			break
+	}
 }
 
 export default handleInvite;
