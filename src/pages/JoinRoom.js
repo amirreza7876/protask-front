7a3a08f94d336a74f69feae14b5428f11a925676
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 16:53:46 +0430

    change room string to request string

--- src/pages/JoinRoom.js
+++ src/pages/JoinRoom.js
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 import searchForRoom from "../utils/searchForRoom";
 
 function JoinRoom(props) {
-	const [roomString, setRoomString] = useState('');
+	const [requestString, setRequestString] = useState('');
 	return (
 		<div>
 			Join Room
-			<form onSubmit={e => searchForRoom(e, roomString)}>
-				<input type="text" name="" value={roomString} onChange={e => setRoomString(e.target.value)} id=""/>
+			<form onSubmit={e => searchForRoom(e, requestString)}>
+				<input type="text" name="" value={requestString} onChange={e => setRequestString(e.target.value)} id=""/>
 				<input type="submit" value={'Send Request'} name="" id=""/>
 			</form>
 		</div>
