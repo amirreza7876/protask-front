0b99f1fee8fd89c62d9f603ff4689736bb7ec297
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-07 13:48:04 +0430

    connected to back with api

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from "../api";
+import {useNavigate} from "react-router-dom";
 
 function CreateRoom(props) {
+	const navigate = useNavigate()
+	const [roomName, setRoomName] = useState('');
+	const [roomString, setRoomString] = useState('');
+	const [roomIsCreated, setRoomIsCreated] = useState(false);
+
+	const handleCreateRoom = async (e) => {
+		e.preventDefault()
+		const response = await api.post('/rooms/create/', {'name': roomName})
+		if (response.status === 200) {
+			setRoomString(response.data.request_string)
+			setRoomIsCreated(true)
+		}
+	}
+
+	if (roomIsCreated) {
+		navigate('/invite', {state: {status: "NEW_ROOM", roomString}})
+	}
+
 	return (
 		<div>
 			Create room
+			<form onSubmit={e => handleCreateRoom(e)}>
+				<input type="text" name="" value={roomName} onChange={e => setRoomName(e.target.value)} id=""/>
+				<input type="submit" value={'Create Room'} name="" id=""/>
+			</form>
 		</div>
 	);
 }
