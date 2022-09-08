ddcfbe68a86016ffc02e97b3d485c8b47fceb239
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-08 12:55:01 +0430

    add tailwind to forms

--- src/pages/CreateRoom.js
+++ src/pages/CreateRoom.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import api from "../api";
 import {useNavigate} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 
 function CreateRoom(props) {
 	const navigate = useNavigate()
@@ -22,13 +23,24 @@ function CreateRoom(props) {
 	}
 
 	return (
-		<div>
-			Create room
-			<form onSubmit={e => handleCreateRoom(e)}>
-				<input type="text" name="" value={roomName} onChange={e => setRoomName(e.target.value)} id=""/>
-				<input type="submit" value={'Create Room'} name="" id=""/>
-			</form>
-		</div>
+		<MainLayout>
+			<div>
+				<form className="m-auto w-full max-w-sm" onSubmit={e => handleCreateRoom(e)}>
+					<div className="flex items-center border-b border-[#4f46e5] py-2">
+						<input
+							className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+							type="text" value={roomName} onChange={e => setRoomName(e.target.value)}
+							placeholder="Name The Board"
+							aria-label="Full name"/>
+						<button
+							className="flex-shrink-0 bg-[#4f46e5] hover:bg-[#2e2990] text-md text-white py-1 px-2 rounded"
+							type="submit">
+							Create
+						</button>
+					</div>
+				</form>
+			</div>
+		</MainLayout>
 	);
 }
 
--- src/pages/JoinRoom.js
+++ src/pages/JoinRoom.js
@@ -1,17 +1,45 @@
 import React, {useState} from 'react';
 import searchForRoom from "../utils/searchForRoom";
+import MainLayout from "../layouts/MainLayout";
+import {InformationCircleIcon} from "@heroicons/react/20/solid";
 
 function JoinRoom(props) {
 	const [requestString, setRequestString] = useState('');
 	return (
-		<div>
-			Join Room
-			<form onSubmit={e => searchForRoom(e, requestString)}>
-				<input type="text" name="" value={requestString} onChange={e => setRequestString(e.target.value)} id=""/>
-				<input type="submit" value={'Send Request'} name="" id=""/>
-			</form>
-		</div>
-	);
+		// <div>
+		// 	Join Room
+		// 	<form onSubmit={e => searchForRoom(e, requestString)}>
+		// 		<input type="text" name="" value={requestString} onChange={e => setRequestString(e.target.value)}
+		// 			   id=""/>
+		// 		<input type="submit" value={'Send Request'} name="" id=""/>
+		// 	</form>
+		// </div>
+		<MainLayout>
+			<div>
+				<form className="m-auto w-full max-w-sm" onSubmit={e => searchForRoom(e, requestString)}>
+					<div className="flex items-center border-b border-[#4f46e5] py-2">
+						<input
+							className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+							type="text" value={requestString} onChange={e => setRequestString(e.target.value)}
+							placeholder="Type Board Request String"
+							aria-label="Full name"/>
+						<button
+							className="flex-shrink-0 bg-[#4f46e5] hover:bg-[#2e2990] text-md text-white py-1 px-2 rounded"
+							type="submit">
+							Join Board
+						</button>
+					</div>
+					<div className={'flex text-gray-500 mt-2'}>
+						<InformationCircleIcon
+							className={'h-4 w-4 mr-1'}/>
+						<p className={'text-sm'}>Team leader must've sent you a request string to join their
+							team.</p>
+					</div>
+				</form>
+			</div>
+		</MainLayout>
+	)
+		;
 }
 
 export default JoinRoom;
