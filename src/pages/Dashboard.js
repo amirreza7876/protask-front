fcdb7a071f60f08f0269e1b7bdd9a094db413234
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-09 00:59:21 +0430

    append related pages to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,26 +1,68 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {userContext} from "../userContext";
-import {Link, Navigate} from "react-router-dom";
+import {Link, Navigate, Outlet, useLocation, useNavigate} from "react-router-dom";
+import getUserName from "../apiCalls/getUserName";
+import NavigationBar from "../components/NavigationBar";
 
 function Dashboard() {
+	const navigate = useNavigate()
+	const {pathname} = useLocation()
+	const currentPage = pathname.split('/')[2]
+	const [username, setUsername] = useState('initState');
+	if(!currentPage){
+		console.log(currentPage)
+		navigate('profile')
+	}
+
+	useEffect(() => {
+		getUserName(setUsername)
+	}, []);
+
 	const {isAuthenticated} = useContext(userContext)
 	if (isAuthenticated) {
-		return (
-			<div
-				className="w-48 text-sm font-medium rounded-md text-gray-900 bg-white border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-				<Link to={'/'}
-					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-500">
-					Home
-				</Link>
-
-
+		return (<div className={'w-screen'}>
+			<NavigationBar/>
+			<div className={'h-52 relative bg-slate-100'}>
+				<div className={'m-auto flex w-1/2 justify-center absolute top-1/3 left-1/4'}>
+					{/*TODO dynamic background color*/}
+					<div
+						className={`bg-indigo-800 align-middle p-2 w-12 h-12 leading-loose items-center text-center rounded-full self-center justify-center items-center`}>
+						<p className={'font-bold text-white'}>
+							{username[0].toUpperCase()}
+						</p>
+					</div>
+					<h2 className={'self-center font-bold ml-4 text-gray-600'}>
+						@{username}
+					</h2>
+				</div>
 			</div>
-		)
-			;
+			<div className={'bg-slate-100 w-full'}>
+				<ul className="flex flex-wrap w-fit m-auto text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+					<li className="mr-2">
+						<Link to="profile" aria-current="page"
+							  className={`inline-block p-4 rounded-t-lg ${currentPage === 'profile' ? "text-blue-600 bg-white" : 'hover:text-blue-600 hover:bg-gray-200'}`}
+						>Profile
+						</Link>
+					</li>
+					<li className="mr-2">
+						<Link to="activity"
+							  className={`inline-block p-4 rounded-t-lg ${currentPage === 'activity' ? "text-blue-600 bg-white" : 'hover:text-blue-600 hover:bg-gray-200'}`}
+						>Activity
+						</Link>
+					</li>
+					<li className="mr-2">
+						<Link to="settings"
+							  className={`inline-block p-4 rounded-t-lg ${currentPage === 'settings' ? "text-blue-600 bg-white" : 'hover:text-blue-600 hover:bg-gray-200'}`}
+						>Settings
+						</Link>
+					</li>
+				</ul>
+			</div>
+			<Outlet/>
+
+		</div>);
 	} else {
-		return (
-			<Navigate to={'/login'}/>
-		)
+		return (<Navigate to={'/login'}/>)
 	}
 }
 
