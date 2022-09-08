07743f70cdc8c82f73f1d3672780fe67e5b7a67b
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-08 12:55:29 +0430

    remove items and moved to navigation bar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,26 +12,8 @@ function Dashboard() {
 					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-500">
 					Home
 				</Link>
-				<Link to={'/create-room'}
-					  className="block py-2 px-4 w-full text-white bg-blue-400 border-b border-gray-200 cursor-pointer dark:bg-gray-600 dark:border-gray-600 hover:bg-gray-500">
-					Create room
-				</Link>
-				<Link to={'/join-room'}
-					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-600 dark:border-gray-600 hover:bg-gray-500">
-					JoinRoom
-				</Link>
-				<Link to={'/my-rooms'}
-					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-600 dark:border-gray-600 hover:bg-gray-500">
-					My rooms
-				</Link>
-				<Link to={'/my-requests'}
-					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-600 dark:border-gray-600 hover:bg-gray-500">
-					Join Requests
-				</Link>
-				<Link to={'/my-invitations'}
-					  className="block py-2 px-4 w-full text-white bg-blue-700 border-b border-gray-200 cursor-pointer dark:bg-gray-600 dark:border-gray-600 hover:bg-gray-500">
-					Join Invites
-				</Link>
+
+
 			</div>
 		)
 			;
