57d2bab76597ca73c26f1184b6d80a78c98483c5
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-10 16:13:31 +0430

    update username and bio

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import getUserName from "../apiCalls/getUserName";
+import getUserData from "../apiCalls/getUserData";
+import handleUpdateUserData from "../apiCalls/handleUpdateUserData";
 
 function Profile(props) {
 	const [username, setUsername] = useState('');
+	const [bio, setBio] = useState('');
 	useEffect(() => {
-		getUserName(setUsername)
+		getUserData(setUsername, null, setBio)
 	}, []);
 
 	return (
@@ -13,7 +15,7 @@ function Profile(props) {
 				<label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">
 					Username
 				</label>
-				<input type="text" id="base-input" value={username}
+				<input type="text" id="base-input" value={username} onChange={e=>setUsername(e.target.value)}
 					   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
 				    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
 			</div>
@@ -21,9 +23,14 @@ function Profile(props) {
 				<label htmlFor="large-input"
 					   className="block mb-2 text-sm font-medium text-gray-900">Bio</label>
 				<textarea id="large-input" rows={5}
-						  className="block w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300
+						  value={bio}
+						  onChange={(e)=>setBio(e.target.value)}
+						  className="block w-full text-gray-900 bg-gray-50 p-2.5 rounded-lg border border-gray-300
 				    sm:text-md focus:ring-blue-500 focus:border-blue-500"/>
 			</div>
+			<button className={'hover:bg-slate-100 text-blue-500 p-3 rounded-lg'} onClick={()=> handleUpdateUserData(username, bio)}>
+				Save
+			</button>
 		</div>
 	);
 }
