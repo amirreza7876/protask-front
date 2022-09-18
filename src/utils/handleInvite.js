3262726dd6462ffa3cd1a6ef7cc526e2b0559089
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-19 02:03:46 +0430

    change request data from request string to board uuid

--- src/utils/handleInvite.js
+++ src/utils/handleInvite.js
@@ -2,15 +2,16 @@ import api from "../api";
 
 const handleInvite = async (event, type, invite) => {
 	event.preventDefault()
-	const fromRoom = invite.from_room.request_string
+	console.log(invite.from_room)
+	const uuid = invite.from_room.id
 	const forUser = invite.for_user
 	const inviteId = invite.id
 	switch (type) {
 		case'accept':
-			await api.post('/rooms/invite/', {fromRoom, forUser, inviteId, accepted: true})
+			await api.post('/rooms/invite/', {uuid, forUser, inviteId, accepted: true})
 			break
 		case 'reject':
-			await api.post('/rooms/invite/', {fromRoom, forUser, inviteId, accepted: false})
+			await api.post('/rooms/invite/', {uuid, forUser, inviteId, accepted: false})
 			break
 	}
 }
