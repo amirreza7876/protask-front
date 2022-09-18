c4e31272ba1b8c64ebd51a16a842e9064e2de670
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-19 02:01:35 +0430

    change sidebar props

--- src/pages/RoomDetail.js
+++ src/pages/RoomDetail.js
@@ -1,45 +1,33 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
-import {Link, Navigate, Outlet, useParams} from "react-router-dom";
+import {Navigate, Outlet, useParams} from "react-router-dom";
 import getRoomDetail from "../apiCalls/getRoomDetail";
 import {userContext} from "../userContext";
 import MainLayout from "../layouts/MainLayout";
-import {
-	ArrowDownIcon, Bars3CenterLeftIcon, ChevronDownIcon, ChevronUpIcon, UserCircleIcon
-} from "@heroicons/react/20/solid";
 import RoomDetailSidebar from "../components/RoomDetailSidebar";
 import getRoomTasks from "../apiCalls/getRoomTasks";
-import TaskCard from "../components/TaskCard";
-import TaskList from "../components/TaskList";
 
-function RoomDetail(props) {
+function RoomDetail() {
 	const {id} = useParams()
-	const [roomDetail, setRoomDetail] = useState({});
-	const loadedRef = useRef(false);
-	const [tasks, setTasks] = useState([]);
-	const [showMembers, setShowMembers] = useState(false);
 	const {isAuthenticated} = useContext(userContext)
+	const [roomDetail, setRoomDetail] = useState({});
 
 	useEffect(() => {
 		getRoomDetail(setRoomDetail, id)
 	}, []);
-
-	if (Object.keys(roomDetail).length !== 0 && !loadedRef.current) {
-		getRoomTasks(setTasks, id, roomDetail.data.request_string)
-		loadedRef.current = true
-	}
 
 
 	if (isAuthenticated) {
 		if (roomDetail.status === 404) {
 			return (<Navigate to={'/dashboard'}/>)
 		} else if (Object.keys(roomDetail).length !== 0) {
-			return (<MainLayout>
-				<div className={'grid grid-cols-5'}>
-					<RoomDetailSidebar roomDetail={roomDetail} showMembers={showMembers}
-									   setShowMembers={setShowMembers}/>
-					<Outlet/>
-				</div>
-			</MainLayout>);
+			return (
+				<MainLayout>
+					<div className={'grid grid-cols-5'}>
+						<RoomDetailSidebar setRoomDetail={setRoomDetail} roomDetail={roomDetail}/>
+						<Outlet/>
+					</div>
+				</MainLayout>
+			);
 		}
 	} else {
 		return (<Navigate to={'/login'}/>)
