872c27d9ffc83a78238bd2b1fff3dac02232a422
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-19 01:51:00 +0430

    create leave board and remove user api calls, modal for remove user, seperate component for users

--- src/apiCalls/changeBoardName.js
+++ src/apiCalls/changeBoardName.js
@@ -2,6 +2,5 @@ import api from "../api";
 
 const changeBoardName = async (id, name) => {
 	const response = await api.patch('/rooms/edit-name/' + id.toString(), {name})
-	console.log(response)
 }
 export default changeBoardName;
--- src/apiCalls/getRoomInvites.js
+++ src/apiCalls/getRoomInvites.js
@@ -1,7 +1,7 @@
 import api from "../api";
 
-const getRoomRequests = async (setRoomInvites, requestString, id) => {
-	const response = await api.get(`/rooms/room-invites/${id}/${requestString}`).catch(err => err.response)
+const getRoomRequests = async (setRoomInvites, id) => {
+	const response = await api.get(`/rooms/room-invites/${id}/`).catch(err => err.response)
 	setRoomInvites({data: response.data, status: response.status})
 }
 export default getRoomRequests;
