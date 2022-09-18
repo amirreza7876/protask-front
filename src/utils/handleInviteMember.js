7e48208694d88bc34ca5400e1f9066fbdb263c6a
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-19 02:04:24 +0430

    remove extra things

--- src/utils/handleInviteMember.js
+++ src/utils/handleInviteMember.js
@@ -1,9 +1,7 @@
 import api from "../api";
 
-const handleInviteMember = async (event, username, requestString) => {
-	event.preventDefault()
-	const response = await api.post('/rooms/join-invites/', {usernameOrEmail: username, requestString})
-	console.log(response)
+const handleInviteMember = async (username, requestString) => {
+	await api.post('/rooms/join-invites/', {usernameOrEmail: username, requestString})
 }
 
 export default handleInviteMember;
