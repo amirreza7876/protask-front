daad223e79e24719a2ba43840c8a8fe82de2283d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-20 03:10:05 +0430

    add task data to task card

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import {ArrowTopRightOnSquareIcon, CheckIcon} from "@heroicons/react/20/solid";
+import priorityStatus from "../utils/checkTaskPriorityStatus";
+import taskStatus from "../utils/checkTaskStatus";
 
 function TaskCard({task}) {
 	console.log(task)
-	return (<div className={`border-l-4
+	return (
+		<tr className={`border-l-4 border-t border-t-slate-200
 				${task.difficulty === 'el' && 'border-cyan-500'}
 				${task.difficulty === 'e' && 'border-emerald-500'}
 				${task.difficulty === 'm' && 'border-yellow-400'}
-				${task.difficulty === 'h' && 'border-red-600'}`}
-	>
-		<div
-			className={`p-3 mt-3 max-w-sm bg-white rounded-r-lg border border-gray-200 shadow-md bg-indigo-100`}>
-			<div className={'flex relative'}>
-				<h5 className="text-lg tracking-tight text-gray-900	">
+				${task.difficulty === 'h' && 'border-red-600'}`}>
+			<td className={'pl-3'}>
+				<h5>
 					{task.title}
 				</h5>
-				{task.done && <CheckIcon className={'ml-4 text-emerald-600 h-6 w-6 self-center absolute right-0'}/>}
-			</div>
+			</td>
+			<td>
+				<p>{task.duration}</p>
+			</td>
+			<td>
+				<p>{task.user.username}</p>
+			</td>
+			<td>
+				<p>{priorityStatus(task.priority)}</p>
+			</td>
+			<td>
+				<p>{taskStatus(task.status)}</p>
+			</td>
+			<td>
+				<input type="checkbox" value={task.done} name="" id=""/>
+			</td>
 
-			<button className="inline-flex items-center text-blue-600 hover:underline"
-					onClick={() => alert('create modal for this')}>
-				Show More Details
-				<ArrowTopRightOnSquareIcon className="ml-2 w-5 h-5"/>
-			</button>
-		</div>
-	</div>);
+		</tr>
+	);
 }
 
 export default TaskCard;
