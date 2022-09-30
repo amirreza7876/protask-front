78c694c0d1c3d5ae33301b53cadd6752c4449812
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-30 16:28:30 +0330

    create script to check full string of status

--- src/utils/checkTaskDifficultyStatus.js
+++ src/utils/checkTaskDifficultyStatus.js
@@ -1,6 +1,17 @@
-const checkTaskDifficultyStatus = (character) => {
+const difficultyStatus = (character) => {
 	switch (character) {
-
+		case 'e':
+			return 'Easy'
+			break
+		case 'el':
+			return "Elementary"
+			break
+		case 'm':
+			return 'Medium'
+			break
+		case 'h':
+			return 'Hard'
+			break
 	}
 }
-export default checkTaskDifficultyStatus;
+export default difficultyStatus;
