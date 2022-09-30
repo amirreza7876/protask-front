b2354b18680eabd7919f89a3033919e66b795637
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-09-30 16:27:56 +0330

    update add task modal and connect it to api

--- src/components/modals/AddTaskModal.js
+++ src/components/modals/AddTaskModal.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import handleLeaveBoard from "../../apiCalls/handleLeaveBoard";
+import addTask from "../../apiCalls/addTask";
+import getRoomTasks from "../../apiCalls/getRoomTasks";
 
-function AddTaskModal({showModal, setShowModal, members}) {
+function AddTaskModal({showModal, id, setTasks, request_string, setShowModal, members}) {
+	const [title, setTitle] = useState('');
+	const [duration, setDuration] = useState('0');
+	const [user, setUser] = useState('');
+	const [difficulty, setDifficulty] = useState('');
+	const [priority, setPriority] = useState('');
+
+	const handleNewTask = async () => {
+		const response = await addTask(title, duration, user, difficulty, priority, id)
+		setTitle('')
+		setDuration('0')
+		setUser('')
+		setDifficulty('')
+		setPriority('')
+		if (response.status === 201) {
+			await getRoomTasks(setTasks, id, request_string)
+			setShowModal(false)
+		}
+	}
+
 	return (
 		showModal ? (<>
 			<div
@@ -18,47 +39,63 @@ function AddTaskModal({showModal, setShowModal, members}) {
 						<div className={'w-2/3'}>
 							<div className={'space-y-3 justify-center'}>
 								<div>
-									<label htmlFor="task_title" className={'text-slate-200 block'}>Name Your Task</label>
+									<label htmlFor="task_title" className={'text-slate-200 block'}>Name Your
+										Task</label>
 									<input type="text"
 										   className={'bg-slate-800 w-full text-slate-200 p-1 px-2 border-0 outline-0 rounded-md'}
+										   value={title}
+										   onChange={e => setTitle(e.target.value)}
 										   name="task_title" id=""/>
 								</div>
 								<div>
 									<label htmlFor="duration" className={'text-slate-200 block'}>Duration</label>
 									<input type="text"
 										   className={'bg-slate-800 w-full text-slate-200 p-1 px-2 border-0 outline-0 rounded-md'}
+										   value={duration}
+										   onChange={e => setDuration(e.target.value)}
 										   name="duration" id=""/>
 								</div>
 								<div className={'w-full'}>
 									<label htmlFor="task_title" className={'text-slate-200'}>Assigned to</label>
 									<select name="members"
+											value={user}
+											onChange={e => setUser(e.target.value)}
 											className={'bg-slate-800 w-full text-slate-200 p-1 px-2 border-0 outline-0 rounded-md'}
 											id="">
 										{members.map(member => (
 											<option value={member.username}>{member.username}</option>
 										))}
+										<option selected={true} value={''}>-- Select Member --</option>
 									</select>
 								</div>
 								<div className={'grid'}>
 									<label htmlFor="difficulty" className={'text-slate-200'}>Difficulty</label>
 									<select name="difficulty"
 											className={'bg-slate-800 text-slate-200 p-1 px-2 border-0 outline-0 rounded-md'}
+											value={difficulty}
+											onChange={e => setDifficulty(e.target.value)}
 											id="">
-										<option value="elementary">Elementary</option>
-										<option value="easy">Easy</option>
-										<option value="medium">Medium</option>
-										<option value="hard">Hard</option>
+										<option value="el">Elementary</option>
+										<option value="e">Easy</option>
+										<option value="m">Medium</option>
+										<option value="h">Hard</option>
+										<option selected={true} value={''}>-- Set Difficulty --</option>
+
 									</select>
 								</div>
 								<div className={'grid'}>
 									<label htmlFor="priority" className={'text-slate-200'}>Priority</label>
 									<select name="priority"
 											className={'bg-slate-800 text-slate-200 p-1 px-2 border-0 outline-0 rounded-md'}
+											value={priority}
+											onChange={e => setPriority(e.target.value)}
 											id="">
-										<option value="low">Low</option>
-										<option value="medium">Medium</option>
-										<option value="major">Major</option>
-										<option value="showstopper">Showstopper</option>
+										<option value="l">Low</option>
+										<option value="me">Medium</option>
+										<option value="ma">Major</option>
+										<option value="sh">Showstopper</option>
+										<option selected={true} value={''}>-- Set Priority --</option>
+
 									</select>
 								</div>
 							</div>
@@ -73,7 +110,7 @@ function AddTaskModal({showModal, setShowModal, members}) {
 								<button
 									className="text-green-500 bg-slate-800 rounded background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 									type="button"
-									onClick={() => setShowModal(false)}
+									onClick={handleNewTask}
 								>
 									Add
 								</button>
