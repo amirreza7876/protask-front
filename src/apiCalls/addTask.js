0454af4ba549e757d7b982dd203cd0e11ef11d3d
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-06 22:09:06 +0330

    add phase field

--- src/apiCalls/addTask.js
+++ src/apiCalls/addTask.js
@@ -1,7 +1,8 @@
 import api from "../api";
 
-const addTask = async (title, duration, user, difficulty, priority, id) => {
-	const response = await api.post('/tasks/create/', {title, duration, user, difficulty, priority, id})
+const addTask = async (title, duration, user, difficulty, priority, id, selectedPhase) => {
+	// console.log(selectedPhase)
+	const response = await api.post('/tasks/create/', {title, duration, user, difficulty, priority, id, selectedPhase})
 	return {status: response.status}
 }
 
