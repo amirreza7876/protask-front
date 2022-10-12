a462d148cffc18d737b401805f92ab944202c3bd
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-13 02:27:23 +0330

    remove request string from params

--- src/apiCalls/getRoomPhases.js
+++ src/apiCalls/getRoomPhases.js
@@ -1,7 +1,7 @@
 import api from "../api";
 
-const getRoomTasks = async (setPhases, setSelectedPhase, roomId, requestString) => {
-	const response = await api.get(`/tasks/room-phases/`, {params: {roomId, requestString}})
+const getRoomTasks = async (setPhases, setSelectedPhase, roomId) => {
+	const response = await api.get(`/tasks/room-phases/`, {params: {roomId}})
 	setSelectedPhase(response.data[0])
 	setPhases(response.data)
 	return response.data
