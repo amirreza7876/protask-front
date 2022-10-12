c1a3c7b2b7c157494c59c032f532f47b92a93a36
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-13 02:29:36 +0330

    update tasks and phases after modification

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,69 +1,84 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TaskCard from "./TaskCard";
 import getRoomDetail from "../apiCalls/getRoomDetail";
 import getRoomTasks from "../apiCalls/getRoomTasks";
 import {useParams} from "react-router-dom";
-import {userContext} from "../userContext";
 import {PlusIcon} from "@heroicons/react/20/solid";
-import AddTagModal from "./modals/AddTagModal";
 import AddTaskModal from "./modals/AddTaskModal";
 import getRoomPhases from "../apiCalls/getRoomPhases";
 import AddPhaseModal from "./modals/AddPhaseModal";
-import {ChevronRightIcon} from "@heroicons/react/24/outline";
 import EditTaskModal from "./modals/EditTaskModal";
+import DeletePhaseModal from "./modals/DeletePhaseModal";
 
 function TaskList() {
 	const {id} = useParams()
 	const [roomDetail, setRoomDetail] = useState({});
-	// const taskLoadedRef = useRef(false);
 	const [showEditModal, setShowEditModal] = useState(false);
-	const phaseLoadedRef = useRef(false);
 	const [tasks, setTasks] = useState([]);
 	const [phases, setPhases] = useState([]);
 	const [showModal, setShowModal] = useState(false);
+	const [showDeletePhaseModal, setShowDeletePhaseModal] = useState(false);
 	const [showPhaseModal, setShowPhaseModal] = useState(false);
 	const [selectedPhase, setSelectedPhase] = useState('');
-	const [update, setUpdate] = useState(true);
 
-
-	if (Object.keys(roomDetail).length !== 0 && !phaseLoadedRef.current) {
-		getRoomPhases(setPhases, setSelectedPhase, id, roomDetail.data.request_string).then(r => {
-		})
-		phaseLoadedRef.current = true
-	}
-
-	const callGetTasks = async () => {
-
-		if (Object.keys(roomDetail).length !== 0 && update) {
-			await getRoomTasks(setTasks, selectedPhase, id, roomDetail.data.request_string)
-			setUpdate(false)
+	const callGetPhases = async () => {
+		if (Object.keys(roomDetail).length !== 0) {
+			await getRoomPhases(setPhases, setSelectedPhase, id, roomDetail.data.request_string)
 		}
 	}
-	const changePhase = (selectedId) => {
+	const callGetTasks = async () => {
+		if (Object.keys(roomDetail).length !== 0) {
+			await getRoomTasks(setTasks, selectedPhase, id)
+			console.log('done')
+		}
+	}
+	const changePhase = async (selectedId) => {
 		const filtered = phases.find(phase => phase.id === selectedId)
 		setSelectedPhase(filtered)
 		setTasks([])
-		getRoomTasks(setTasks, filtered, id, roomDetail.data.request_string).then(r => {
-		})
+		await getRoomTasks(setTasks, filtered, id, roomDetail.data.request_string)
+	}
+	const handleDeletePhase = () => {
+		setShowDeletePhaseModal(true)
 	}
 
 	useEffect(() => {
-		callGetTasks()
+		callGetTasks().then(r => {
+		})
 	}, [phases]);
-
 	useEffect(() => {
-		getRoomDetail(setRoomDetail, id).then(r => {
+		callGetPhases().then(r => {
 		})
 		callGetTasks().then(r => {
 		})
+	}, [roomDetail]);
+	useEffect(() => {
+		getRoomDetail(id).then(r => {
+			setRoomDetail({data: r.data, status: r.status})
+		})
 	}, []);
+
 	return (
 		<div className={'col-span-4 ml-3'}>
-			<AddTaskModal selectedPhase={selectedPhase} showModal={showModal} setTasks={setTasks}
+			<AddTaskModal selectedPhase={selectedPhase}
+						  showModal={showModal}
+						  setTasks={setTasks}
 						  request_string={roomDetail.data?.request_string}
-						  id={id} setShowModal={setShowModal} members={roomDetail.data?.members}/>
-			<AddPhaseModal showPhaseModal={showPhaseModal} setPhases={setPhases} setShowPhaseModal={setShowPhaseModal}
-						   request_string={roomDetail.data?.request_string} id={id}/>
+						  id={id}
+						  setShowModal={setShowModal}
+						  members={roomDetail.data?.members}/>
+			<AddPhaseModal showPhaseModal={showPhaseModal}
+						   setPhases={setPhases}
+						   setSelectedPhase={setSelectedPhase}
+						   setShowPhaseModal={setShowPhaseModal}
+						   id={id}/>
+			<DeletePhaseModal showDeletePhaseModal={showDeletePhaseModal}
+							  setShowDeletePhaseModal={setShowDeletePhaseModal}
+							  selectedPhase={selectedPhase}
+							  setSelectedPhase={setSelectedPhase}
+							  setPhases={setPhases}
+							  roomId={id}
+			/>
 			<div className={'bg-slate-900 text-white -ml-3 flex justify-between'}>
 				<div className={'flex gap-4'}>
 					<button className={'flex hover:bg-indigo-600 p-4'} onClick={() => setShowModal(true)}>
@@ -76,14 +91,14 @@ function TaskList() {
 					</button>
 				</div>
 				<div className={'flex'}>
-					<button className={'flex hover:bg-indigo-600 p-4'}>
+					<button className={'flex hover:bg-indigo-600 p-4'} onClick={handleDeletePhase}>
 						<p>Delete Phase</p>
 					</button>
 				</div>
 			</div>
 			<div
 				className={'flex gap-2 mt-3 overflow-x-auto py-2 mr-3 rounded border-b-2 text-gray-600 '}>
-				{phases.length !== 0 &&
+				{phases.length !== 0 ?
 					phases.map(phase => (
 						<button key={phase.id}
 								onClick={() => changePhase(phase.id)}
@@ -95,7 +110,11 @@ function TaskList() {
 							</p>
 
 						</button>
-					))}
+					)) :
+					(
+						<p>no phases</p>
+					)
+				}
 			</div>
 
 			<div className={'grid grid-cols-3'}>
@@ -114,9 +133,20 @@ function TaskList() {
 						</thead>
 						{tasks.length !== 0 && tasks.map(task => (
 							<>
-								<EditTaskModal showEditModal={showEditModal} setShowEditModal={setShowEditModal}
-											   task={task} members={roomDetail.data?.members}/>
-								<TaskCard setUpdate={setUpdate} setShowEditModal={setShowEditModal} members={roomDetail.data?.members} task={task}/>
+								<EditTaskModal
+									showEditModal={showEditModal}
+									setShowEditModal={setShowEditModal}
+									members={roomDetail.data?.members}
+									task={task}
+									id={id}
+									setTasks={setTasks}
+									selectedPhase={selectedPhase}
+								/>
+								<TaskCard setShowEditModal={setShowEditModal}
+										  callGetTasks={callGetTasks}
+										  roomId={id}
+										  task={task}
+								/>
 							</>
 						))}
 					</table>
