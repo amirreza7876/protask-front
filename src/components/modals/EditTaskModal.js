b90b43eadde15380c3ece2b2abc20a8c130182e1
amirreza <amirreza.taherkhani.2012@gmail.com>
2022-10-14 02:58:34 +0330

    nothing important

--- src/components/modals/EditTaskModal.js
+++ src/components/modals/EditTaskModal.js
@@ -1,20 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import handleLeaveBoard from "../../apiCalls/handleLeaveBoard";
 import {ExclamationTriangleIcon} from "@heroicons/react/24/outline";
 import changeTask from "../../apiCalls/changeTask";
 import getRoomTasks from "../../apiCalls/getRoomTasks";
 
-function EditModalTask({showEditModal, setShowEditModal, task, members, id, setTasks, selectedPhase}) {
-	const [title, setTitle] = useState(task.title);
-	const [duration, setDuration] = useState(task.duration);
-	const [user, setUser] = useState(task.user.username);
-	const [difficulty, setDifficulty] = useState(task.difficulty);
-	const [priority, setPriority] = useState(task.priority);
+function EditModalTask({showEditModal, setShowEditModal, editableTask, members, id, setTasks, selectedPhase}) {
+	console.log(editableTask)
+	const [title, setTitle] = useState(editableTask.title);
+	const [duration, setDuration] = useState(editableTask.duration);
+	const [user, setUser] = useState(editableTask.user.username);
+	const [difficulty, setDifficulty] = useState(editableTask.difficulty);
+	const [priority, setPriority] = useState(editableTask.priority);
+
+	useEffect(() => {
+		setTitle(editableTask.title)
+		setDuration(editableTask.duration)
+		setUser(editableTask.user.username)
+		setDifficulty(editableTask.difficulty)
+		setPriority(editableTask.priority)
+	}, [editableTask]);
+
+
 	const applyChanges = async () => {
-		await changeTask({title, duration, user, difficulty, priority}, task.id)
+		await changeTask({title, duration, user, difficulty, priority}, editableTask.id)
 		setShowEditModal(false)
 		//TODO update task after update
-		await getRoomTasks(setTasks, selectedPhase, id)
+		const response = await getRoomTasks(setTasks, selectedPhase, id)
 	}
 	return (showEditModal ? (<>
 		<div
